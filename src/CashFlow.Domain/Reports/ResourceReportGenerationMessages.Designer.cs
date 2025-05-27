ad8e41df4e6c415072ebb158764d2fa129dd45bf
Andrew da Silva <andrewairamdasilva@gmail.com>
2025-05-26 23:46:31 -0300

    feat(domain): add resource report messages

--- src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
+++ src/CashFlow.Domain/Reports/ResourceReportGenerationMessages.Designer.cs
@@ -97,6 +97,15 @@ namespace CashFlow.Domain.Reports {
         }
         
         /// <summary>
+        ///   Consulta uma cadeia de caracteres localizada semelhante a Hey, N1z0!.
+        /// </summary>
+        public static string GREETINGS {
+            get {
+                return ResourceManager.GetString("GREETINGS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Consulta uma cadeia de caracteres localizada semelhante a Payment Type.
         /// </summary>
         public static string PAYMENT_TYPE {
